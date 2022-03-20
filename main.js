fcf97be80fad210a4ddcc2254b1e9dba7734a1a0
tanatswanashe-zuze <tanatswanashe.zuze@student.moringaschool.com>
2022-03-20 23:28:33 +0200

    constuctors and prototypes not working

--- main.js
+++ main.js
@@ -1,76 +1,35 @@
-//pizza names//
+function Pizza(name, size, topping) {
+    this.name = name;
+    this.size = size;
+    this.topping = topping;
 
 
+};
+Pizza.prototype.sizes = {
+    small: 10,
+    medium: 15,
+    large: 20
+};
 
+Pizza.prototype.names = {
+    magarita: 20,
+    Neapolitan: 15,
+    luis: 13
+};
 
-function myF() {
+Pizza.prototype.toppings = {
+    pineapple: 2,
+    cheese: 3,
+    sauce: 1,
+};
 
-    function getName() {
-        var PName = document.getElementById("name")
-        document.getElementById("totalp").innerHTML = PName
-        if (PName = "Neapolitan")
-            var PPrice = 10
-        else if (PName = "greek")
-            PPrice = 7
-        else if (PName = "magarita")
-            PPrice = 5
-        else if (PName = "luis")
-            PPrice = 10
-
-    }
-
-
-    //bases//
-    function getbase() {
-        base = document.getElementById("base").value
-
-        if (base = "normal")
-            BPrice = 2
-        else if (base = "thick")
-            BPrice = 4
-        else if (base = "thin")
-            Bprice = 3
-    }
-
-
-
-    //sizes//
-
-    function getsize() {
-        size = document.getElementById("size").value
-
-        if (size = "Small")
-            SPrice = 8
-        else if (size = "Medium")
-            SPrice = 10
-        else if (size = "Large")
-            Sprice = 12
-    }
-
-    $(document).ready(function() {
-            $(".pineapple").select(function() {
-                tprice = 1
-
-            });
-            $(document).ready(function() {
-                    $(".cheese").select(function() {
-                        tprice = 2
-
-                    });
-                    $(document).ready(function() {
-                            $(".sauce").select(function() {
-                                tprice = 3
-
-                            });
-
-                        }
-
-                        /function calcTotal() {
-
-                        Total = PPrice + SPrize + T1Price + T2Price + T3Price + BPrice
-
-
-                    })
-            })
-    })
+Pizza.prototype.price = function() {
+    let Nameprice = this.names[this.name];
+    let Sizeprice = this.sizes[this.size];
 }
+
+let pizzaprice = this.name + this.size;
+console.log(pizzaprice)
+
+let pizza1 = new Pizza(magarita, small, cheese)
+console.log(pizza1.pizzaprice)
